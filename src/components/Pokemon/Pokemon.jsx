74e681b9e97f64d54632cccfdda884a9e8b8e7f7
Manish Chand <manishchandr349@gmail.com>
2023-08-14 13:18:23 +0530

    adding Css ing pokemonDetails

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 function Pokemon({name, image,id}) {
   return (
     <div className='pokemon'>
+     <div className='pokemon-name'>{name}</div>
     <Link to={`/pokemon/${id}`}>
 
-     <div className='pokemon-name'>{name}</div>
      <div><img className='pokemon-img' src={image} alt="" /></div>
     </Link>
     </div>
